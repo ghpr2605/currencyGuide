07efe6190091653309762131e0f56991214c188d
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2022-02-09 21:23:44 +0530

    Added state and API logic

--- currency-guide/src/App.js
+++ currency-guide/src/App.js
@@ -2,10 +2,23 @@ import React, { Component } from "react";
 import Table from "./Table";
 
 class App extends Component {
+  state = {
+    currencyDetails: [],
+  };
+
+  componentDidMount() {
+    const url = "https://api.coinbase.com/v2/currencies";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then(({ data }) => {
+        this.setState({ currencyDetails: data });
+      });
+  }
   render() {
-    return <Table></Table>;
+    const { currencyDetails } = this.state;
+    return <Table displayData={currencyDetails}></Table>;
   }
 }
 
 export default App;
-
